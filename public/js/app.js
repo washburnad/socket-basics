b2969addcb82596e7553824efbc321c4376972a9
Aaron Washburn <aaron@it-kutch.com>
2017-03-01 16:54:37 -0500

    Add name and join chat screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,20 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to socket io server');
+    // jQuery('.messages').append('<p><strong>' + name + ' has entered ' + room + '.</strong></p>');
 });
 
 socket.on('message', function (message) {
-    var momentTimestamp = moment.utc(message.timestamp).local().format('h:mm a');
-    
+    var momentTimestamp = moment.utc(message.timestamp);
+    var $message = jQuery('.messages');
+
     console.log('New message(' + momentTimestamp + '): ' + message.text);
 
-    jQuery('.messages').append('<p><strong>(' + momentTimestamp + ')</strong> - ' + message.text + '</p>');
+    $message.append('<p></strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
 })
 
 // Handles submitting of new message
@@ -20,6 +25,7 @@ $form.on('submit', function (e) {
     var $messageField = $form.find('input[name=message]')
 
     socket.emit('message', {
+        name: name,
         text: $messageField.val()
     });
 
--- server.js
+++ server.js
@@ -4,25 +4,25 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
-var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
-    var timestamp;
 
     socket.on('message', function (message) {
-        console.log('Message received');
-        console.log(message);
-        message.timestamp = now.valueOf();
+        console.log('Message received ' + message.text);
+
+        message.timestamp = moment().valueOf();
         io.emit('message', message);
     });
 
 
-    socket.emit('message', function () {
-        text: 'Welcome to the chat app'
-        timestamp: now.valueOf();
+    console.log('emit on connection');
+    socket.emit('message', {
+        name: 'System',
+        text: 'Welcome to the chat app',
+        timestamp: moment().valueOf()
     });
 
 });
